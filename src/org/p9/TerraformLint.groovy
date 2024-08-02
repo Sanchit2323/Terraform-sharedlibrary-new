210f9c67f4800cf576725427396efff5d9e01c88
Sanchit2323 <153765874+Sanchit2323@users.noreply.github.com>
2024-08-02 15:58:31 +0530

    Update TerraformLint.groovy

--- src/org/p9/TerraformLint.groovy
+++ src/org/p9/TerraformLint.groovy
@@ -3,7 +3,7 @@ package org.p9
 def call(String tflintPath) {
         catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
         sh """
-        ${tflintPath} --format json > tflint_report.json
+        ${TFLINT_PATH} --format json > tflint_report.json
         """
     }
 }
