bea587b6ba8f7386d51f5e347524d4b285d21832
Sanchit2323 <153765874+Sanchit2323@users.noreply.github.com>
2024-08-02 10:24:41 +0530

    Update CheckovScan.groovy

--- src/org/p9/CheckovScan.groovy
+++ src/org/p9/CheckovScan.groovy
@@ -1,13 +1,11 @@
 package org.p9
 
-class CheckovScan {
-    def call(script, String checkovPath, String workspace) {
-        script.catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
-            script.sh """
-                cd ${workspace}
-                ${checkovPath}/checkov -d . -s --output-file-path checkov_report.json
-            """
-            script.sh 'ls -l'
-        }
+def call(script, String checkovPath, String workspace) {
+    script.catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
+        script.sh """
+            cd ${workspace}
+            ${checkovPath}/checkov -d . -s --output-file-path checkov_report.json
+        """
+        script.sh 'ls -l'
     }
 }
