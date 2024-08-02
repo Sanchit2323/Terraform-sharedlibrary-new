a2b58f5717b4718a25360358c4ff5a1b9f43f214
Sanchit2323 <153765874+Sanchit2323@users.noreply.github.com>
2024-08-02 10:44:10 +0530

    Update terraformPipeline.groovy

--- vars/terraformPipeline.groovy
+++ vars/terraformPipeline.groovy
@@ -1,4 +1,53 @@
-import org.p9.CompileJavaCode
-import org.p9.SonarqubeJava
-import org.p9.JavaTestUtils
-import org.p9.OwaspDependencyCheck
+import org.p9.ArchiveReports
+import org.p9.CheckovScan
+import org.p9.TerraformFormat
+import org.p9.TerraformLint
+import org.p9.TerraformInit
+import org.p9.TerraformValidate
+
+def call(Map params) {
+    def REPO_URL = "https://github.com/Sanchit2323/Terra-module-CI.git"
+
+    def CHECKOV_PATH = '/var/lib/jenkins/.local/bin'
+    def TFLINT_PATH = '/usr/local/bin/tflint' // Adjust the path if tflint is installed elsewhere
+    def WORKSPACE = params.WORKSPACE
+
+    checkoutRepo(REPO_URL)
+    terraformInitCall(this)
+    terraformFormatCall(this)
+    terraformValidateCall(this)
+    terraformLintCall(this, TFLINT_PATH)
+    checkovScanCall(this, CHECKOV_PATH, WORKSPACE)
+    archiveReportsCall(this)
+}
+
+def checkoutRepo(String repoUrl) {
+    script {
+        echo "Checking out repository: ${repoUrl}"
+        git url: "${repoUrl}", branch: 'main'
+    }
+}
+
+def terraformInitCall(script) {
+    new TerraformInit().call(script)
+}
+
+def terraformFormatCall(script) {
+    new TerraformFormat().call(script)
+}
+
+def terraformValidateCall(script) {
+    new TerraformValidate().call(script)
+}
+
+def terraformLintCall(script, String tflintPath) {
+    new TerraformLint().call(script, tflintPath)
+}
+
+def checkovScanCall(script, String checkovPath, String workspace) {
+    new CheckovScan().call(script, checkovPath, workspace)
+}
+
+def archiveReportsCall(script) {
+    new ArchiveReports().call(script)
+}
