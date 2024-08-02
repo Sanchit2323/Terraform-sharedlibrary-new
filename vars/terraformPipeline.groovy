e0c90036e92e066e0f2534bfae2aa1f08bc21043
Sanchit2323 <153765874+Sanchit2323@users.noreply.github.com>
2024-08-02 11:01:09 +0530

    Update terraformPipeline.groovy

--- vars/terraformPipeline.groovy
+++ vars/terraformPipeline.groovy
@@ -8,11 +8,17 @@ import org.p9.TerraformValidate
 def call(Map params) {
     def REPO_URL = "https://github.com/Sanchit2323/Terra-module-CI.git"
 
+    // Use environment variables for paths
     def CHECKOV_PATH = '/var/lib/jenkins/.local/bin'
     def TFLINT_PATH = '/usr/local/bin/tflint' // Adjust the path if tflint is installed elsewhere
-    def WORKSPACE = params.WORKSPACE
 
+    // Using the passed workspace from the params
+    def WORKSPACE = params.WORKSPACE ?: env.WORKSPACE
+
+    // Checkout repository
     checkoutRepo(REPO_URL)
+
+    // Call Terraform operations
     terraformInitCall(this)
     terraformFormatCall(this)
     terraformValidateCall(this)
@@ -29,25 +35,25 @@ def checkoutRepo(String repoUrl) {
 }
 
 def terraformInitCall(script) {
-    new TerraformInit().call(script)
+    new TerraformInit(script).call()
 }
 
 def terraformFormatCall(script) {
-    new TerraformFormat().call(script)
+    new TerraformFormat(script).call()
 }
 
 def terraformValidateCall(script) {
-    new TerraformValidate().call(script)
+    new TerraformValidate(script).call()
 }
 
 def terraformLintCall(script, String tflintPath) {
-    new TerraformLint().call(script, tflintPath)
+    new TerraformLint(script).call(tflintPath)
 }
 
 def checkovScanCall(script, String checkovPath, String workspace) {
-    new CheckovScan().call(script, checkovPath, workspace)
+    new CheckovScan(script).call(checkovPath, workspace)
 }
 
 def archiveReportsCall(script) {
-    new ArchiveReports().call(script)
+    new ArchiveReports(script).call()
 }
