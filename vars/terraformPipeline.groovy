628bbc0de386e4806c74ecf6b8795995e4418c3c
Sanchit2323 <153765874+Sanchit2323@users.noreply.github.com>
2024-08-02 14:54:06 +0530

    Update terraformPipeline.groovy

--- vars/terraformPipeline.groovy
+++ vars/terraformPipeline.groovy
@@ -4,26 +4,7 @@ import org.p9.TerraformFormat
 import org.p9.TerraformLint
 import org.p9.TerraformInit
 import org.p9.TerraformValidate
-
-def call(Map params = [:]) {
-    def REPO_URL = "https://github.com/Sanchit2323/Terra-module-CI.git"
-
-    // Use environment variables for paths
-    def CHECKOV_PATH = '/var/lib/jenkins/.local/bin'
-    def TFLINT_PATH = '/usr/local/bin/tflint' // Adjust the path if tflint is installed elsewhere
-
-    node {
-        checkoutRepo(REPO_URL)
-        terraformInitCall(this)
-        terraformFormatCall(this)
-        terraformValidateCall(this)
-        terraformLintCall(this, TFLINT_PATH)
-        checkovScanCall(this, CHECKOV_PATH, WORKSPACE)
-        archiveReportsCall(this)
-    }    
-}
     
-
 def checkoutRepo(String repoUrl) {
     script {
         echo "Checking out repository: ${repoUrl}"
